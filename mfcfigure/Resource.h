8b48870e5493d0cb5bb490cac183290201dc1d16
Gromov Stas (802) <drygma@gmail.com>
2016-04-08 17:07:27 +0300

    Deleted temporary files
    Deleted temporary files

--- mfcfigure/Resource.h
+++ mfcfigure/Resource.h
@@ -7,6 +7,8 @@
 #define IDR_MAINFRAME                   128
 #define IDR_MFCFigureTYPE               130
 #define IDR_TOOLBAR1                    312
+#define IDD_DIALOG1                     314
+#define IDD_PROPERTIES                  314
 #define ID_BUTTON32771                  32771
 #define ID_NEWFIGURE                    32772
 
@@ -14,7 +16,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        314
+#define _APS_NEXT_RESOURCE_VALUE        315
 #define _APS_NEXT_COMMAND_VALUE         32773
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
